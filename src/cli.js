520318e9d61a6f93569668dcf1571c72d6323a4e
argemiront <argemiront@gmail.com>
2020-05-18 12:13:50 -0700

    init files

---
+++ src/cli.js
@@ -0,0 +1,148 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+import simpleGit from 'simple-git/promise';
+import builder from '../src/main'
+import fs from 'fs';
+
+const localGit = 'Local directory';
+const remoteGit = 'Git remote address';
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      '--git': String,
+      '--dir': String,
+      '--code': String,
+      '--setup': String,
+      '--output': String,
+      '--help': Boolean,
+      '-g': '--git',
+      '-d': '--dir',
+      '-c': '--code',
+      '-s': '--setup',
+      '-o': '--output',
+      '-h': '--help',
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+  return {
+    git: args['--git'],
+    dir: args['--dir'],
+    codeBranch: args['--code'],
+    setupBranch: args['--setup'],
+    output: args['--output'],
+    help: args['--help'] || false,
+  };
+}
+
+async function promptForMissingOptions(options) {
+
+  const questions = [];
+
+  // if no git remote addres is provided, assume current folder
+  if (!options.git && !options.dir) {
+
+    // check if the current dir is a valid repo
+     const git = simpleGit(__dirname);
+    const isRepo = await git.checkIsRepo();
+
+    if (!isRepo) {
+      
+      questions.push({
+        type: 'list',
+        name: 'source',
+        message: `The current directory (${__dirname}) is not a valid git repo. Would you like to provide a...`,
+        choices: [localGit, remoteGit],
+        default: localGit,
+      });
+
+      questions.push({
+        type: 'input',
+        name: 'localGit',
+        message: 'Please, provide a directory of the valid git repository: ',
+        when: (answers) => answers.source === localGit,
+      });
+
+      questions.push({
+        type: 'input',
+        name: 'remoteGit',
+        message: 'Please, provide the address of a remote git repository: ',
+        when: (answers) => answers.source === remoteGit,
+      });
+    }
+  }
+  // if both local dir and remote repos are provided
+  else if (options.git && options.dir) {
+    questions.push({
+      type: 'list',
+      name: 'source',
+      message: 'It was provided both a local git directory and a remote address. Please, choose either one or those to parse: ',
+      choices: [localGit, remoteGit],
+      default: localGit,
+    });
+  }
+
+  // if the branch containing the code is not provided
+  if (!options.codeBranch) {
+    questions.push({
+      type: 'imput',
+      name: 'codeBranch',
+      message: 'Please, provide the branch with the code commits: ',
+    });
+  }
+
+  // if the branch containing the setup files is not provided
+  if (!options.setupBranch) {
+    questions.push({
+      type: 'imput',
+      name: 'setupBranch',
+      message: 'Please, provide the branch with the setup files (coderoad.yaml and tutorial.md): ',
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+
+  let repo;
+  let isLocal;
+
+  if (answers.source) {
+    repo = answers.source === localGit ? options.dir : options.git;
+    isLocal = answers.source === localGit;
+  }
+  else {
+    repo = options.dir || options.git || __dirname;
+    isLocal = options.git ? false : true;
+  }
+
+  return {
+    repo,
+    setupBranch: options.setupBranch || answers.setupBranch,
+    codeBranch: options.codeBranch || answers.codeBranch,
+    output: options.output,
+    isLocal,
+  };
+}
+
+export async function cli(args) {
+  let options = parseArgumentsIntoOptions(args);
+
+  // If help called just print the help text and exit
+  if (options.help) {
+    console.log('help message...');
+  }
+  else {
+    // Otherwise, continue with the other options
+    options = await promptForMissingOptions(options);
+    console.log(options);
+    config = await builder(options);
+
+    if (options.output) {
+      fs.writeFileSync(options.output, config, 'utf8');
+    }
+    else {
+      console.log(JSON.stringify(config))
+    }
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,238 @@
+const simpleGit = require('simple-git/promise');
+const yamlParser = require('js-yaml');
+const path = require('path');
+const _ = require('lodash');
+const fs = require('fs');
+const validate = require('./validator');
+
+const workingDir = 'tmp';
+
+function parseContent(md) {
+
+  let start = -1;
+  const parts = [];
+
+  const lines = md.split('\n');
+
+  // Split the multiple parts - This way enables the creator to use 4/5 level headers inside the content.
+  lines.forEach((line, index) => {
+    if (line.match(/#{1,3}\s/) || index === lines.length - 1) {
+      if (start !== -1) {
+        parts.push(lines.slice(start, index).join('\n'));
+        start = index
+      }
+      else {
+        start = index
+      }
+    }
+  });
+
+  const sections = {};
+
+  // Identify and remove the header
+  const summaryMatch = parts.shift().match(/^#\s(?<tutorialTitle>.*)\n+(?<tutorialDescription>[^]*)/);
+
+  sections['summary'] = {
+    title: summaryMatch.groups.tutorialTitle,
+    description: summaryMatch.groups.tutorialDescription,
+  };
+
+  // Identify each part of the content
+  parts.forEach(section => {
+    const levelRegex = /^(##\s(?<levelId>L\d+)\s(?<levelTitle>.*)\n*>\s+(?<levelSummary>.*)\n+(?<levelContent>[^]*))/;
+    const stepRegex = /^(###\s(?<stepId>(?<levelId>L\d+)S\d+)\s(?<stepTitle>.*)\n+(?<stepContent>[^]*))/;
+
+    const levelMatch = section.match(levelRegex);
+    const stepMatch = section.match(stepRegex);
+
+    if (levelMatch) {
+      const level = {
+        [levelMatch.groups.levelId]: {
+          id: levelMatch.groups.levelId,
+          title: levelMatch.groups.levelTitle,
+          summary: levelMatch.groups.levelSummary,
+          content: levelMatch.groups.levelContent,
+        }
+      };
+
+      _.merge(sections, level);
+
+    }
+    else if (stepMatch) {
+
+      const step = {
+        [stepMatch.groups.levelId]: {
+          steps: {
+            [stepMatch.groups.stepId]: {
+              id: stepMatch.groups.stepId,
+              // title: stepMatch.groups.stepTitle, //Not using at this momemnt
+              content: stepMatch.groups.stepContent
+            }
+          }
+        }
+      };
+
+      _.merge(sections, step);
+
+    }
+
+  });
+
+  return sections;
+
+}
+
+function rmDir(dir, rmSelf) {
+
+  let files;
+  rmSelf = (rmSelf === undefined) ? true : rmSelf;
+  // dir = dir + path.sep;
+
+  try { files = fs.readdirSync(dir); } catch (e) { console.log(`Sorry, directory '${dir}' doesn't exist.`); return; }
+
+  if (files.length > 0) {
+    files.forEach(function (x, i) {
+      if (fs.statSync(path.join(dir, x)).isDirectory()) {
+        rmDir(path.join(dir, x));
+      } else {
+        fs.unlinkSync(path.join(dir, x));
+      }
+    });
+  }
+
+  if (rmSelf) {
+    // check if user want to delete the directory ir just the files in this directory
+    fs.rmdirSync(dir);
+  }
+  
+}
+
+async function cleanupFiles(workingDir) {
+
+  try {
+
+    const gitModule = simpleGit(__dirname);
+
+    await gitModule.subModule(['deinit', '-f', workingDir]);
+    await gitModule.rm(workingDir);
+    await gitModule.reset(['HEAD']);
+    rmDir(path.join(__dirname, '.git', 'modules', workingDir));
+    rmDir(workingDir);
+
+    return true;
+
+  } catch (error) {
+
+    return false;
+
+  }
+
+}
+
+/**
+ * 
+ * @param {string} repo Git url to the repo. It should finish with .git
+ * @param {string} codeBranch The branch containing the tutorial code
+ * @param {string} setupBranch The branch containing the configuration files
+ * @param {string} isLocal define if the repo is local or remote
+ */
+async function build({ repo, codeBranch, setupBranch, isLocal }) {
+  
+  const gitModule = simpleGit(__dirname);
+
+  const startCleanup = cleanupFiles(workingDir);
+
+  if (startCleanup) {
+    console.log('Clenned old files from git module...');
+  }
+
+  await gitModule.submoduleAdd(repo, workingDir);
+
+  const git = simpleGit(path.join(__dirname, workingDir));
+  await git.fetch();
+
+  // checkout the branch to load configuration and content branch
+  await git.checkout(setupBranch);
+
+  // Load files
+  const _mdContent = fs.readFileSync(path.join(__dirname, workingDir, 'TUTORIAL.md'), 'utf8');
+  let _config = fs.readFileSync(path.join(__dirname, workingDir, 'coderoad.yaml'), 'utf8');
+
+  // Add one more line to the content as per Shawn's request
+  const mdContent = parseContent(_mdContent);
+
+  // Parse config to JSON
+  const config = yamlParser.load(_config);
+
+  // Add the summary to the config file
+  config['summary'] = mdContent.summary;
+
+  // merge content and config
+  config.levels.forEach(level => {
+    const { steps, ...content } = mdContent[level.id];
+
+    level.steps.forEach(step => _.merge(step, steps[step.id]));
+
+    _.merge(level, content);
+  });
+
+  // Checkout the code branches
+  await git.checkout(codeBranch);
+
+  // Load all logs
+  const logs = await git.log();
+
+  // Filter relevant logs
+  const parts = new Set();
+
+  for (const commit of logs.all) {
+
+    const matches = commit.message.match(/^(?<stepId>(?<levelId>L\d+)S\d+)(?<stepType>[QA])?/);
+
+    if (matches && !parts.has(matches[0])) {
+      // Uses a set to make sure only the latest commit is proccessed 
+      parts.add(matches[0]);
+
+      // Add the content and git hash to the config
+      if (matches.groups.stepId) {
+        // If it's a step: add the content and the setup/solution hashes depending on the type
+        const theStep = config.levels
+          .find(level => level.id === matches.groups.levelId)
+          .steps.find(step => step.id === matches.groups.stepId);
+
+        if (matches.groups.stepType === 'Q') {
+          theStep.setup.commits.push(commit.hash.substr(0, 7));
+        }
+        else if (matches.groups.stepType === 'A' && theStep.solution.commits) {
+          theStep.solution.commits.push(commit.hash.substr(0, 7));
+        }
+      }
+      else {
+        // If it's level: add the commit hash (if the level has the commit key) and the content to the config
+        const theLevel = config.levels.find(level => level.id === matches.groups.levelId);
+
+        if (_.has(theLevel, 'config.commits')) {
+          theLevel.setup.commits.push(commit.hash.substr(0, 7));
+        }
+      }
+
+    }
+
+  };
+
+  // cleanup the submodules
+  const endCleanup = await cleanupFiles(workingDir);
+  
+  if (!endCleanup){
+    console.log('Error when deleting the git submodules');
+  }
+
+  const isValid = validate(config);
+
+  if (!isValid) return validate.errors;
+  
+  return config;
+
+}
+
+module.exports = build;
