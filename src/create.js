b3b03b96a6676d05dc1bb70883419bbf019d20be
shmck <shawn.j.mckay@gmail.com>
2020-05-30 10:31:23 -0700

    add vscode linting
    Signed-off-by: shmck <shawn.j.mckay@gmail.com>

--- src/create.js
+++ src/create.js
@@ -1,35 +1,30 @@
-import ncp from 'ncp';
-import path from 'path';
-import { promisify } from 'util';
-import * as os from 'os';
+import ncp from "ncp";
+import path from "path";
+import { promisify } from "util";
+import * as os from "os";
 
 const copy = promisify(ncp);
- 
+
 const copyFiles = async (filePath) => {
-
   try {
-
     let filePath = new URL(import.meta.url).pathname;
 
-    if (os.platform() === 'win32') {
+    if (os.platform() === "win32") {
       // removes the leading drive letter from the path
       filePath = filePath.substr(3);
     }
 
-    const templateDirectory = path.resolve(
-      filePath, '..', 'templates',
-    );
+    const templateDirectory = path.resolve(filePath, "..", "templates");
 
     const targetDirectory = process.cwd();
 
     await copy(templateDirectory, targetDirectory, {
       clobber: false,
     });
-  }
-  catch(e) {
-    console.log('Error on creating the files:');
+  } catch (e) {
+    console.log("Error on creating the files:");
     console.log(JSON.stringify(e, null, 1));
   }
-}
+};
 
 export default copyFiles;
