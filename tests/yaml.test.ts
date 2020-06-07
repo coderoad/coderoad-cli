e44942d6478ef8da4e5d7383be291edb1f228fb8
shmck <shawn.j.mckay@gmail.com>
2020-06-07 09:35:04 -0700

    outline yaml validation tests
    Signed-off-by: shmck <shawn.j.mckay@gmail.com>

--- tests/yaml.test.ts
+++ tests/yaml.test.ts
@@ -1,3 +1,18 @@
 describe("yaml", () => {
   it.todo("should parse a valid yaml file");
+  it.todo("should fail if version is invalid");
+  it.todo("should fail if version is missing");
+  it.todo("should fail if config is missing");
+  it.todo("should fail if config testRunner is missing");
+  it.todo("should fail if config testRunner command is missing");
+  it.todo("should fail if config testRunner args tap is missing");
+  it.todo("should fail if repo is missing");
+  it.todo("should fail if repo uri is missing");
+  it.todo("should fail if repo uri is invalid");
+  it.todo("should fail if repo branch is missing");
+  it.todo("should fial if level is missing id");
+  it.todo("should fail if level setup is invalid");
+  it.todo("should fail if step is missing id");
+  it.todo("should fail if step setup is invalid");
+  it.todo("should fail if solution setup is invalid");
 });
