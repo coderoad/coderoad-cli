d4916da5503533740b347a553b5cd31fdabd9e2c
shmck <shawn.j.mckay@gmail.com>
2020-06-12 17:32:49 -0700

    setup validate markdown tests
    Signed-off-by: shmck <shawn.j.mckay@gmail.com>

--- tests/markdown.test.ts
+++ tests/markdown.test.ts
@@ -1,11 +1,103 @@
-import * as T from "../typings/tutorial";
 import { validateMarkdown } from "../src/utils/validateMarkdown";
 
 describe("validate markdown", () => {
-  it.todo("should return false if missing a summary title (#)");
-  it.todo("should return false if contains multiple `#` headers");
-  it.todo("should return false if missing a summary description");
-  it.todo("should return false if `##` doesn't preface a level");
-  it.todo("should return false if `###` doesn't preface a step");
-  it.todo("should return true for valid markdown");
+  it("should return false if missing a summary title (#)", () => {
+    const md = `
+Description.
+
+## L1 Put Level's title here
+
+> Level's summary: a short description of the level's content in one line.
+
+Some text that describes the level`;
+    expect(validateMarkdown(md)).toBe(false);
+  });
+
+  it("should return false if contains multiple `#` headers", () => {
+    const md1 = `# A Title
+Description.
+
+# Another Title
+
+## L1 Put Level's title here
+
+> Level's summary: a short description of the level's content in one line.
+
+Some text that describes the level`;
+
+    const md2 = `# A Title
+Description.
+
+
+## L1 Put Level's title here
+
+> Level's summary: a short description of the level's content in one line.
+
+Some text that describes the level
+
+# Another title
+`;
+    expect(validateMarkdown(md1)).toBe(false);
+    expect(validateMarkdown(md2)).toBe(false);
+  });
+
+  it("should return false if missing a summary description", () => {
+    const md = `# A Title
+
+## L1 Put Level's title here
+
+> Level's summary: a short description of the level's content in one line.
+
+Some text that describes the level
+`;
+    expect(validateMarkdown(md)).toBe(false);
+  });
+
+  it("should return false if `##` doesn't preface a level", () => {
+    const md = `# A Title
+
+A description
+
+## Put Level's title here
+
+> Level's summary: a short description of the level's content in one line.
+
+Some text that describes the level
+`;
+    expect(validateMarkdown(md)).toBe(false);
+  });
+
+  it("should return false if `###` doesn't preface a step", () => {
+    const md = `# A Title
+
+A description
+
+## Put Level's title here
+
+> Level's summary: a short description of the level's content in one line.
+
+Some text that describes the level
+
+### A Step
+
+First step
+`;
+  });
+
+  it("should return true for valid markdown", () => {
+    const md = `# Title
+
+Description.
+
+## L1 Put Level's title here
+
+> Level's summary: a short description of the level's content in one line.
+
+Some text that describes the level
+
+### L1S1
+
+First Step`;
+    expect(validateMarkdown(md)).toBe(true);
+  });
 });
