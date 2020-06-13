40a57fb181a7735af3858f041c4d027814f7d987
shmck <shawn.j.mckay@gmail.com>
2020-06-13 08:31:25 -0700

    switch to fs-extra in validate
    Signed-off-by: shmck <shawn.j.mckay@gmail.com>

--- src/validate.ts
+++ src/validate.ts
@@ -1,16 +1,9 @@
 import * as path from "path";
-import * as fs from "fs";
-import util from "util";
+import * as fs from "fs-extra";
 import * as yamlParser from "js-yaml";
 import { getArg } from "./utils/args";
 import gitP, { SimpleGit } from "simple-git/promise";
 import { getCommits, CommitLogObject } from "./utils/commits";
-import simplegit from "simple-git/promise";
-
-const mkdir = util.promisify(fs.mkdir);
-const exists = util.promisify(fs.exists);
-const rmdir = util.promisify(fs.rmdir);
-const read = util.promisify(fs.readFile);
 
 async function validate(args: string[]) {
   // dir - default .
@@ -22,7 +15,7 @@ async function validate(args: string[]) {
     yaml: getArg(args, { name: "yaml", alias: "y" }) || "coderoad.yaml",
   };
 
-  const _yaml = await read(path.join(localDir, options.yaml), "utf8");
+  const _yaml = await fs.readFile(path.join(localDir, options.yaml), "utf8");
 
   // parse yaml config
   let skeleton;
@@ -42,19 +35,22 @@ async function validate(args: string[]) {
   const codeBranch: string = skeleton.config.repo.branch;
 
   // validate commits
-  const commits = await getCommits({ localDir, codeBranch });
+  const commits: CommitLogObject = await getCommits({ localDir, codeBranch });
+  console.log("commits", commits);
 
   // setup tmp dir
   const tmpDir = path.join(localDir, ".tmp");
 
   try {
-    if (!(await exists(tmpDir))) {
-      await mkdir(tmpDir);
+    if (!(await fs.pathExists(tmpDir))) {
+      await fs.emptyDir(tmpDir);
     }
     const tempGit: SimpleGit = gitP(tmpDir);
     await tempGit.init();
 
     // VALIDATE TUTORIAL TESTS
+    if (commits.INIT) {
+    }
 
     // run test runner setup command(s)
     // loop over commits:
@@ -75,7 +71,7 @@ async function validate(args: string[]) {
     console.error(e.message);
   } finally {
     // cleanup
-    await rmdir(tmpDir);
+    await fs.emptyDir(tmpDir);
   }
 }
 
